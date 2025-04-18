73723d1274f3ce492e547937802f11f957637056
Darwin Froese <froese.darwin@gmail.com>
2025-04-17 22:10:07 -0500

    fixes references in colors/prairie.lua

--- colors/prairie.lua
+++ colors/prairie.lua
@@ -14,7 +14,7 @@ vim.g.colors_name = "prairie"
 --
 -- The performance impact of this call can be measured in the hundreds of
 -- *nanoseconds* and such could be considered "production safe".
-package.loaded["lush_theme.prairie"] = nil
+package.loaded["prairie"] = nil
 
 -- include our theme file and pass it to lush to apply
-require("lush")(require("lush_theme.prairie"))
+require("lush")(require("prairie"))
