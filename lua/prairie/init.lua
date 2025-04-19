5693b3456e58bb0d3a5be005ed1fb4e06092e051
Darwin Froese <froese.darwin@gmail.com>
2025-04-18 22:13:23 -0500

    clears namespace hl group for final test

--- lua/prairie/init.lua
+++ lua/prairie/init.lua
@@ -303,7 +303,7 @@ local theme = lush(function(injected_functions)
 		sym("@preproc")({ PreProc }), -- PreProc
 		sym("@debug")({ Debug }), -- Debug
 		sym("@tag")({ Tag }), -- Tag
-		sym("@lsp.type.namespace")({ bg = unset }),
+		sym("@lsp.type.namespace")({}),
 	}
 end)
 
